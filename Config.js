8d92a449b8fc89ec1104e962afd392b95784156c
Asaduzzaman <asaduzzaman.cse@gmail.com>
2019-07-07 03:54:58 +0600

    update api url

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 
 //const API_URL = "http://127.0.0.1:8000"
-const API_URL = "http://theasad.pythonanywhere.com"
+const API_URL = "https://theasad.pythonanywhere.com"
 const Config = {
     API_BASE_URL: `${API_URL}/api/v1/drive/folders/`
 }

e71dbe93d3dad0534188193dd24247d0c629a465
Asaduzzaman <asaduzzaman.cse@gmail.com>
2019-07-07 01:38:47 +0600

    update api

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 
-const API_URL = "http://127.0.0.1:8000"
-// const API_URL = "http://18.219.88.76:8005"
+//const API_URL = "http://127.0.0.1:8000"
+const API_URL = "https://mdrive-api-v1.herokuapp.com"
 const Config = {
     API_BASE_URL: `${API_URL}/api/v1/drive/folders/`
 }

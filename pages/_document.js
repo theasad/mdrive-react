a90e9d751edeb6cc3f24fd0c3a5a84521041a33b
Asaduzzaman <asaduzzaman.cse@gmail.com>
2019-07-07 02:24:18 +0600

    update font

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,8 @@ class MyDocument extends Document {
                     {/* PWA primary color */}
                     <meta name="theme-color" content={theme.palette.primary.main} />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                        rel="stylesheet" />
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
